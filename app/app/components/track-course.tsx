9cc73f2940cf2d46800308a01a6c78b51dec054a
Александр Куваев <vinatorul@yandex-team.ru>
2026-09-21 23:51:26 +0300

    Выровнять ширину дорожек и поправить развилки брусчатки

--- app/app/components/track-course.tsx
+++ app/app/components/track-course.tsx
@@ -8,9 +8,9 @@ import { Scenery } from './scenery';
 
 const PARK_TRAILS = [
   { material: 'soil', path: 'M16 22 C24 22 22 8 32 8 C43 8 47 16 59 16' },
-  { material: 'pavers', path: 'M16 78 C8 82 10 92 23 91 C35 90 39 83 52 87 C66 93 81 96 91 86 C99 77 93 66 79 65' },
-  { material: 'pavers', path: 'M16 40 C11 40 8 44 8 50 C8 56 11 60 16 60' },
-  { material: 'pavers', path: 'M8 50 C6 64 13 72 23 70 C27 69 30 69 34 69' },
+  { material: 'pavers', path: 'M16 78 C25 78 23 91 35 91 C43 91 47 87 55 87 C67 87 67 65 79 65' },
+  { material: 'pavers', path: 'M16 60 C23 60 22 72 29 72 C38 72 34 54 45 54 C54 54 52 43 61 43' },
+  { material: 'pavers', path: 'M30 22 C36 22 38 39 46 39 C54 39 57 23 65 23 C73 23 73 35 79 35' },
   { material: 'soil', path: 'M16 40 C22 40 22 48 28 48 C32 48 32 41 36 41' },
   { material: 'soil', path: 'M59 16 C67 16 73 18 77 26 C81 34 82 50 90 50' },
   { material: 'soil', path: 'M48 69 C54 69 54 81 61 81 C69 81 71 65 79 65' },
@@ -60,8 +60,8 @@ function CourseLines({ tracks, aspect }: { tracks: Track[]; aspect: number }) {
     >
       <PathMaterials id={id} aspect={aspect} />
       <ParkTrails id={id} />
-      <ParkTrails id={id} surface />
       <CoursePaths tracks={tracks} id={id} />
+      <ParkTrails id={id} surface />
       <CoursePaths tracks={tracks} id={id} surface />
     </svg>
   );
