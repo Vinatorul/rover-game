5a7ddf9520cb6ff1371a8943a306de416cd6757f
Александр Куваев <vinatorul@yandex-team.ru>
2026-09-21 23:42:18 +0300

    Добавить три грунтовые тропинки в парк

--- app/app/components/track-course.tsx
+++ app/app/components/track-course.tsx
@@ -11,6 +11,9 @@ const PARK_TRAILS = [
   { material: 'pavers', path: 'M16 78 C8 82 10 92 23 91 C35 90 39 83 52 87 C66 93 81 96 91 86 C99 77 93 66 79 65' },
   { material: 'pavers', path: 'M16 40 C11 40 8 44 8 50 C8 56 11 60 16 60' },
   { material: 'pavers', path: 'M8 50 C6 64 13 72 23 70 C27 69 30 69 34 69' },
+  { material: 'soil', path: 'M9 11 C3 18 3 28 8 33 C10 36 13 40 16 40' },
+  { material: 'soil', path: 'M59 16 C66 11 74 15 76 21 C79 28 75 31 70 31' },
+  { material: 'soil', path: 'M48 69 C42 71 40 77 44 82 C46 85 49 86 52 87' },
 ];
 
 function useTrackAspect() {
